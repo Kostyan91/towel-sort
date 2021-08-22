d6ac207282b183045fc2f9e03c1e51292ca0847f
Kostyan91 <kostyan912404@gmail.com>
2021-08-22 13:32:21 +0400

    fead add soluttion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
+// let a = [
+//   [1,2,3],
+//   [4,5,6],
+//   [7,8,9],
+// ];
+
 module.exports = function towelSort (matrix) {
+
+for (let i =0; i<matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++){
+    let columnIndex = i % 2 ===0
+    ? j 
+    : (matrix[i].length -j -1);
+    console.log( matrix[i] [columnIndex])
+  } 
+}    
+
   return [];
 }
