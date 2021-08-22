d3225823ffb6902795b2c43a8da2c1e06176720f
Kostyan91 <kostyan912404@gmail.com>
2021-08-22 17:02:34 +0400

    fead fff

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@
 module.exports = function towelSort (matrix) {
   let g = [];
   
-    if (typeof(matrix) == !Array || matrix == 0) return g
+    if (typeof(matrix) == !Array || matrix == null) return g
     for (let i = 0; i < matrix.length; i++) {
       if (i % 2 !== 0) {
         g.push((matrix[i]).reverse());
