6992fd9204ccc88cd7a42b2d835a148b53a473c1
Kostyan91 <kostyan912404@gmail.com>
2021-08-22 16:55:34 +0400

    fead fff

--- src/index.js
+++ src/index.js
@@ -19,30 +19,7 @@ module.exports = function towelSort (matrix) {
       } else g.push(matrix[i]);
 
     }
-  
-  
-
   return (g.flat())
-
   }
-module.exports = function towelSort (matrix) {
-  try{
-        if(matrix.length==0){
-             return [];
-            }
-            let flattend = [];
-            (function flat(matrix){
-              for (let i = 0; i < matrix.length; i++) {
-                if(i%2!=0){
-                  matrix[i].reverse();
-                }
-                  flattend.push(matrix[i]);
-              }
-            })(matrix);
-            return flattend.flat();
-
-}
-    return [];
-}
 
 
